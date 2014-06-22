ff5c39b58a78d7bc37e5d70c410f448b094ffc60
arbennett <bennett.andr@gmail.com>
2014-06-21 21:53:41 -0400

    Started the switch over to PIL from PyGame.

--- GeneticImage.py
+++ GeneticImage.py
@@ -1,14 +1,23 @@
 '''
-A quick image generator using genetic algorithms
+An image generator using genetic algorithms
 to mimic input files
 
 Created on May 13, 2014
 
 @author: Andrew Bennett
 '''
+#########################################################################################################
+# TODO: 
+#   Fix the size of the converted arrays so that it's all 
+#    in one dimension and the right size.
+#########################################################################################################
 
+
+#-------------------------------------------------------------------------
 # Imports
-import pygame
+#-------------------------------------------------------------------------
+from PIL import Image, ImageDraw
+import numpy
 import time
 import sys
 import random
@@ -17,7 +26,9 @@ import numpy
 import argparse
 import copy
 
-# Program operation stuff
+#-------------------------------------------------------------------------
+# Command line options
+#-------------------------------------------------------------------------
 parser = argparse.ArgumentParser()
 parser.add_argument("-L", "--loadImage", help="The name of the imagefile to load (including file extension)", 
                     default="testImage.jpg")
@@ -29,15 +40,12 @@ parser.add_argument("-E", "--elements", help="The number of elements per individ
                     type=float, default=1e3)
 args=parser.parse_args()
 
-# Start up pygame & display window
-pygame.init()
-image = pygame.image.load(args.loadImage)
-size = width, height = image.get_size()
 
-screen = pygame.display.set_mode(size)
-
-
-# Variables
+#-------------------------------------------------------------------------
+# Variable declarations
+#-------------------------------------------------------------------------
+image =Image.open(args.loadImage)
+size = width, height = image.size
 pop = args.population
 Ngen = args.generations
 Nelements = int(args.elements)
@@ -45,42 +53,49 @@ Nreproduce = height/10
 rMax = height/10 # Maximum circle radius
 diffMax=height/10
 
-
+#-------------------------------------------------------------------------
 # Function definitions
+#-------------------------------------------------------------------------
 
-# PROBLEM: pygame.draw.circle doesn't return a reference to the image but a pygame rect
-#
+# Convert the image to an array 
+# Supposedly this is the fastest way
+def imageToArray(image):
+    imArr = numpy.fromstring(image.tostring(), dtype=numpy.uint8)
+    numpy.reshape(imArr,(height,width))
+    return 0 
+
 # Generates a random test
 def generateRandomTest():
     testElements=[]
+    testImg = Image.new('RGB',(width,height),"white")
+    draw = ImageDraw.Draw(testImg)
     for i in range(Nelements):
         r=int(rMax*random.random())
         x, y=random.randint(0,width), random.randint(0,height)
         val=random.randint(0,255)
         theColor=(val, val, val)
-        testElements.append([r,x,y,val])
-        testImage=pygame.draw.circle(screen, theColor, (x,y), r)    
-    return testElements, pygame.surfarray.array3d(testImage)
+        testElements.append([r,x,y,val])  
+        draw.ellipse((x-r,y-r,x+r,y+r), fill=theColor)
+    return testElements, list(testImg.getData())
 
-# PROBLEM: pygame.draw.circle doesn't return a reference to the image but a pygame rect
-#
 # Input specs: [ [ r, x, y, val ], ..., [ r, x, y, val ] ]
 def generateSeededTest(input):
     testElements=[]
+    testImg = Image.new('RGB',(width,height),"white")
+    draw = ImageDraw.Draw(testImg)
     for i in range(Nelements):
         r=int(input[i,0]+random.random()-0.5)
         x, y=input[i,1]+random.randint(), input[i,2]+random.randint()
         val=input[i,3]+random.randint(-5,5)
         theColor=(val, val, val)
-        testElements.append([x,y,r,val])
-        testImage=pygame.draw.circle(screen, theColor, (x,y), r)    
-    return testElements, pygame.surfarray.array3d(testImage)
+        testElements.append([x,y,r,val])   
+        draw.ellipse((x-r,y-r,x+r,y+r), fill=theColor)
+    return testElements, list(testImg.getData())
         
-# Thanks stackoverflow - see if we can do a better job
-#  I'm beginning to think pygame is a waste of time except for the saving of images
-#  Maybe use PIL instead - I hear good things about that.        
-def grayscale(img):
-    arr = pygame.surfarray.array3d(img)
+# Convert the image to grayscale for now - handling color shouldn't be too bad
+# Also returns in array form     
+def grayscale(img): 
+    arr = imageToArray(img)
     avgs = [[(r*0.298 + g*0.587 + b*0.114) for (r,g,b) in col] for col in arr]
     return numpy.array([[[avg] for avg in col] for col in avgs])
 
@@ -93,16 +108,22 @@ def calculateScore(mimic, actual):
     return score
     
 
+#-------------------------------------------------------------------------
 # Execute the algorithm
+#-------------------------------------------------------------------------
 imageArr=grayscale(image)
 # Generate the seed population
 samples=[]
-scores=[0]*Nreproduce
+scores=[Infinity]*Nreproduce
 lowScore=255*height*width
+
 for each in range(pop):
+    # Start with completely random set
     params, testArr=generateRandomTest()
-    score=calculateScore(testArr, imageArr)
-    # If we found a new low score
+    score=calculateScore(testArr, imageArr)    
+    # If we found a new low score append the list of parameters
+    # Then keep the score list sorted (should probably make sure
+    # the list stays sorted in a more efficient way)
     if score < scores[-1]:
         scores[-1]=score
         samples=sorted(samples)
@@ -112,8 +133,7 @@ for each in range(pop):
 for gen in range(Ngen):
     seeds=copy.deepcopy(samples)
     samples=[]
-    scores=[0]*Nreproduce
-    lowScore=Infinity
+    scores=[Infinity]*Nreproduce
     
     #Generate the sample population and keep the top matches
     for each in range(pop):
@@ -125,6 +145,10 @@ for gen in range(Ngen):
             samples=sorted(samples)
             samples.append(params)
     # save the final image(s) from each generation
+    for each in samples:
+        saveImg = Image.fromarray(each)
+        saveImg.save("genetic"+each+"_"+gen, "JPEG")
+        
 
 
     
